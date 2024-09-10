5e3f3c1a4291a9442a180f99afe944a2cc61e098
antalfiabenjamin <benjaminantalfia@gmail.com>
2024-09-10 22:12:23 +0200

    argo test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Local Openshift Cluster!'
+    return 'Hello Local Openshift Cluster (update test)!'
 
 
 if __name__ == '__main__':
